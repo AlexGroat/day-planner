6d8cf931961a13bd9c31761a401a5c2de65f9462
Alex Groat <alex.groat.95@gmail.com>
2021-10-09 16:35:10 +1030

    Fourth commit js finished and stays on screen, color scheme still needs work

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,27 @@
 var appointText = "";
 var appointTime = "";
-var todaysDate;
+var currentDate;
 var currentTime;
 var currentContainer;
 var tempArray = [];
-var bookedSessions;
-var returnedSessions;
+var storedAppointments;
+var returnedAppointments;
 
 
 
 $(window).on("load", function () {
-    todaysDate = moment().format("dddd MMM Do YYYY, h:mm a");
-    $("#currentDay").append(todaysDate);
+    currentDate = moment().format("dddd MMM Do YYYY, h:mm a");
+    $("#currentDay").append(currentDate);
     currentTime = moment().format("H");
 
-    function renderSessions() {
-        bookedAppointment = JSON.parse(localStorage.getItem("appointments"));
-        if (bookedSessions !== null) {
-            for (i = 0; i < bookedSessions.length; i++) {
-                returnedSessions = bookedSessions[i];
-                details = returnedSessions.details;
-                timeIndex = returnedSessions.time;
-                timeIndex = timeIndex.replace(":00", "");
+    function renderAppointments() {
+        storedAppointments = JSON.parse(localStorage.getItem("appointments"));
+        if (storedAppointments !== null) {
+            for (i = 0; i < storedAppointments.length; i++) {
+                returnedAppointments = storedAppointments[i];
+                details = returnedAppointments.details;
+                timeIndex = returnedAppointments.time;
+                timeIndex = timeIndex.replace(":00", '');
                 if (details !== null) {
                     $("#" + timeIndex).children('div').children('div').children('textarea').val(details);
                 }
@@ -29,5 +29,42 @@ $(window).on("load", function () {
         }
     }
 
-renderSessions();
+    renderAppointments();
+
+    for (i = 0; i <= 23; i++) {
+        CurrentContainer = i;
+        if (currentTime == i) {
+            $('#' + CurrentContainer).addClass("present");
+            $('#' + CurrentContainer).children('div').children('div').children("textarea").addClass("present");
+        }
+        else if (currentTime > i) {
+            $('#' + CurrentContainer).addClass("past");
+            $('#' + CurrentContainer).children('div').children('div').children("textarea").addClass("past");
+        }
+        else {
+            $('#' + CurrentContainer).addClass("future");
+            $('#' + CurrentContainer).children('div').children('div').children("textarea").addClass("future");
+        }
+    }
+})
+
+
+
+$(".saveBtn").click(function () {
+    appointText = $(this).parent('div').children('div').children('textarea').val();
+    appointTime = $(this).parent('div').parent().attr("id");
+    appointment = {
+        time: appointTime,
+        details: appointText
+    }
+    tempArray = JSON.parse(localStorage.getItem("appointments"));
+    if (tempArray === null) {
+        localStorage.setItem('appointments', JSON.stringify([{ time: appointTime, details: appointText }]));
+    }
+    else {
+        tempArray.push(appointment);
+        localStorage.setItem("appointments", JSON.stringify(tempArray));
+
+    }
+    $(this).parent('div').children('div').children('textarea').replaceWith($('<textarea>' + appointText.addClass("textarea") + '</textarea>'));
 })
